29e92cbd11685c99edc38211bd2bdef9ae66877b
victornobrega00 <victornobrega007@gmail.com>
2026-08-05 00:07:16 -0300

    Nova aplicação do meu projeto

---
+++ src/main/java/com/victor/dolistapi/DoListApiApplication.java
@@ -0,0 +1,13 @@
+package com.victor.dolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoListApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoListApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/victor/dolistapi/DoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.dolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoListApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
