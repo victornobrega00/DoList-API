978c840e07593a47722400b895281cca4404e9e2
victornobrega00 <victornobrega007@gmail.com>
2026-08-06 14:21:04 -0300

    Commit do AuthorizationService

---
+++ src/main/java/com/victor/dolistapi/infrastructure/AuthorizationService.java
@@ -0,0 +1,21 @@
+package com.victor.dolistapi.infrastructure;
+
+import com.victor.dolistapi.domain.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public AuthorizationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o e-mail: " + username));
+    }
+}
